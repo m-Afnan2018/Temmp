2e1168cc219223a96c3d04472bc2d5ae05d2519b
m.Afnan <m.afnan2018@gmail.com>
2023-07-26 12:16:41 +0000

    Start successfully

--- Server/server.js
+++ Server/server.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
 
+app.get('/', (req, res)=>{
+    res.status(200).json({
+        success: true,
+        message: 'Server Started Successfully',
+    })
+})
+
 app.listen(4000, ()=>{
     console.log('Server is running at port 4000');
 })
